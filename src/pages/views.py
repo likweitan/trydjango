05437c8bfcc1c1c9b886ed05aba2ce59f2bd76b9
likweitan <likweitan@gmail.com>
2021-02-06 12:19:43 +0800

    Rendering Context in a Template

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,3 +13,12 @@ def home_view(request, *args, **kwargs):
 
 def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [1, 2, 3]
+    }
+    return render(request, "about.html", my_context)
