754cfb0e427f59020c0de4fcbfda12d244fdfa3d
likweitan <likweitan@gmail.com>
2021-02-06 18:11:01 +0800

    How Django Templates Load with Apps

--- src/products/views.py
+++ src/products/views.py
@@ -10,4 +10,4 @@ def product_detail_view(request):
     context = {
         'object': obj
     }
-    return render(request, "product/detail.html", context)
+    return render(request, "products/product_detail.html", context)
