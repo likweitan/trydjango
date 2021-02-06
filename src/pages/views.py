27f6c66d20cec92f4b73184167e55207e5f1528c
likweitan <likweitan@gmail.com>
2021-02-06 11:35:19 +0800

    Django Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -7,8 +7,9 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
