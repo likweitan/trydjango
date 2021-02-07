1d2a9c8cd130858b657e028f1662be6211aa68b1
likweitan <likweitan@gmail.com>
2021-02-07 11:31:53 +0800

    View of a List of Database Objects

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,13 +17,15 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import product_detail_view, product_create_view, dynamic_lookup_view, product_delete_view
+from products.views import (product_detail_view, product_create_view,
+                            dynamic_lookup_view, product_delete_view, product_list_view)
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('contact/', contact_view,),
     path('about/', about_view,),
     path('product/', product_detail_view,),
+    path('products/', product_list_view, name='product-list'),
     path('products/<int:id>/', dynamic_lookup_view,),
     path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
     path('create/', product_create_view,),
