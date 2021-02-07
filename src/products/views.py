d378cb8e32455a14496858bfb320050f35975991
likweitan <likweitan@gmail.com>
2021-02-07 11:24:03 +0800

    Delete and Confirm

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import ProductForm, RawProductForm
 
@@ -74,3 +74,16 @@ def dynamic_lookup_view(request, id):
         'object': obj
     }
     return render(request, "products/product_detail.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    # POST request
+    if request.method == "POST":
+        # confirming delete
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_delete.html", context)
